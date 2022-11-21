660afebb65a2aa22d67e206803e2374562417d78
sbahala <sbahala@stevens.edu>
2022-11-22 03:28:03 +0530

    Added the Attendance module

--- mysas/SAS/views/userinfo.py
+++ mysas/SAS/views/userinfo.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from SAS.models import UserInfo
+from SAS.models import UserInfo, Attendance , AttendanceTotal
 
 
 def student(request):
@@ -20,7 +20,16 @@ def changePassword(request):
     return render(request, "changePassword.html")
 
 def viewAttendance(request):
-    return render(request, "viewAttendance.html")
+    #stud = UserInfo.objects.get(id)
+
+    context = {}
+    all = Attendance.objects.all()
+    #all = []
+    context["attendance"] = all
+    #a = AttendanceTotal()#tried to add after attendance calculation but needs more work
+    #a.save()
+    #all.append(a)
+    return render(request, "viewAttendance.html",context)
 
 def bookSlot(request):
     return render(request, "bookSlot.html")
